01641ac2ff69d18b0e388f22ec965e3dfc91b5f9
Yamid <yamid.arenas@openenglish.com>
2013-04-19 14:11:10 -0300

    soo ready

--- js/custom.js
+++ js/custom.js
@@ -1,10 +1,6 @@
-var userLat = "";
-var userLong = "";
-//var map="";
-
 var myApp = {
-    map:{},
-	getLocation: function () {
+    map: {},
+    getLocation: function () {
         if (navigator.geolocation) {
             // timeout at 60000 milliseconds (60 seconds)
             var options = {
@@ -23,25 +19,25 @@ var myApp = {
     },
 
     initialize: function (latitude, longitude) {
-		var myLatlng = new google.maps.LatLng(latitude, longitude);
-		
-		var mapOptions = {
-			zoom: 12,
-			center: myLatlng,
-			mapTypeId: google.maps.MapTypeId.ROADMAP
-		}
+        var myLatlng = new google.maps.LatLng(latitude, longitude);
 
-		map = new google.maps.Map(document.getElementById("map-canvas"), mapOptions);
+        var mapOptions = {
+            zoom: 12,
+            center: myLatlng,
+            mapTypeId: google.maps.MapTypeId.ROADMAP
+        }
 
-		var marker = new google.maps.Marker({
-			position: myLatlng,
-			map: map,
-			title:"My Location"
-		});
+        map = new google.maps.Map(document.getElementById("map-canvas"), mapOptions);
+
+        var marker = new google.maps.Marker({
+            position: myLatlng,
+            map: map,
+            title: "My Location"
+        });
 
 
         myApp.partners.loadJson();
-    },  
+    },
 
     partners: {
         loadJson: function () {
@@ -51,29 +47,30 @@ var myApp = {
                 dataType: 'json',
                 success: function (data) {
                     $.each(data, function (ind) {
-                    	var partnerLatLng = new google.maps.LatLng(data[ind].lat, data[ind].lng);
-                    	
-                    	var markerNear = new google.maps.Marker({
-							//map: map,
-					        animation: google.maps.Animation.DROP,
-							position: partnerLatLng,
-							title:"partners"
-						});
-						markerNear.setMap(map);
+                        $(".availist ul").append("<li>" + data[ind].name + "</li>");
+                        var partnerLatLng = new google.maps.LatLng(data[ind].lat, data[ind].lng);
 
-					    var contentString = data[ind].name + " <br> " + data[ind].description;
+                        var markerNear = new google.maps.Marker({
+                            //map: map,
+                            animation: google.maps.Animation.DROP,
+                            position: partnerLatLng,
+                            title: "partners"
+                        });
+                        markerNear.setMap(map);
 
-					    var infowindow = new google.maps.InfoWindow({
-					        content: contentString
-					    });
+                        var contentString = data[ind].name + " <br> " + data[ind].description;
 
-					    google.maps.event.addListener(markerNear, 'mouseover', function() {
-					        infowindow.open(map, markerNear);
-					    });
+                        var infowindow = new google.maps.InfoWindow({
+                            content: contentString
+                        });
 
-					     google.maps.event.addListener(markerNear, 'mouseout', function() {
-					        infowindow.close(map, markerNear);
-					    });
+                        google.maps.event.addListener(markerNear, 'mouseover', function () {
+                            infowindow.open(map, markerNear);
+                        });
+
+                        google.maps.event.addListener(markerNear, 'mouseout', function () {
+                            infowindow.close(map, markerNear);
+                        });
 
 
 
@@ -81,13 +78,9 @@ var myApp = {
                 }
             });
         },
-        drawNears: function(partnerLatLng){
-        	console.log(partnerLatLng);
-        	
-        }
     },
 
-	errorHandler: function (err) {
+    errorHandler: function (err) {
         if (err.code == 1) {
             alert("Error: Access is denied!");
         } else if (err.code == 2) {
@@ -97,15 +90,79 @@ var myApp = {
 
 };
 
-var eventos ={
-    inicio: function(){
-        $("#mCanvas").hide();
+var eventos = {
+    inicio: function () {
+        //$("#map-canvas").hide();
+
+        $("#makeRoom").hide();
+        $("#createRoom").hide();
+    },
+    roomList: function () {
+        $("#makeRoom").show();
+        // $("#mCanvas").show();
+        //$(".availist").slideDown();
+    },
+
+    createRoom: function () {
+        $("#createRoom").show();
         $(".availist").hide();
-        
+        $("#mCanvas").hide();
+    },
+
+    elSearch: function () {
+
+        $(".availist").slideDown();
+
     }
 }
 
 $(function () {
+    myApp.initialize();
     myApp.getLocation();
     eventos.inicio();
+
+    $("#search").on("click", function () {
+        //$(".availist").hide();
+        $("#mCanvas").show();
+        eventos.elSearch();
+        $(".participants").hide();
+
+    });
+    $(".roomL").on("click", function () {
+        eventos.inicio();
+        $("#lInput").hide();
+        eventos.createRoom();
+        $(".participants").hide();
+    });
+    $(".findP").on("click", function () {
+        eventos.inicio();
+        $("#lInput").show();
+        $(".participants").hide();
+    });
+
+    $(".createR").on("click", function () {
+        $(".availist").hide();
+        $("#mCanvas").hide();
+        $("#createRoom").hide();
+        $("#makeRoom").show();
+        $("#lInput").hide();
+        $(".participants").hide();
+
+    });
+
+    $(".join").on("click", function () {
+        $("#createRoom").hide();
+        $(".participants").show();
+
+    });
+    $("#start-conferencing").on("click", function () {
+        $("#makeRoom").hide();
+
+    })
+
+    $(".logo").on("click", function () {
+
+        window.location.href = "http://10.50.30.14/hton/";
+
+    })
 })
